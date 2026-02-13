a35a667d60e62e7e4bac1fa1c98828cf0bfab2e5
0Cesar0123 <cesar.wo111213@gmail.com>
2026-02-12 19:51:08 -0600

    Agregar página "Acerca de César" y enlace en index.php

---
+++ acerca_de.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acerca de César</title>
+</head>
+<body>
+    <h1>Acerca de César</h1>
+    <p>César es un programador apasionado con una amplia experiencia en desarrollo de software. Ha trabajado en diversos proyectos, desde aplicaciones web hasta sistemas de inteligencia artificial. Su dedicación y habilidades lo han llevado a ser reconocido como uno de los mejores programadores del mundo.</p>
+    <p>Además de su trabajo en programación, César también es un mentor activo en la comunidad tecnológica, compartiendo su conocimiento a través de conferencias, talleres y publicaciones en línea. Su objetivo es inspirar a la próxima generación de programadores y contribuir al avance de la tecnología.</p> 
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -9,6 +9,8 @@
     <h1>
         Bienvenido a la página de César, el mejor programador del mundo. Aquí encontrarás todo lo relacionado con su trabajo, proyectos y logros.
     </h1>
+
+    <a href="/acerca_de.php"> Acerca de</a>
     
 </body>
 </html>
